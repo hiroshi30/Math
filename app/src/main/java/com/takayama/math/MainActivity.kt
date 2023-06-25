bc570defc27cb97d5a53a5cd4e1d422e3d1ad1f7
takayama3 <imtakabaka@gmail.com>
2023-06-25 21:21:12 +0900

    commit

--- app/src/main/java/com/takayama/math/MainActivity.kt
+++ app/src/main/java/com/takayama/math/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SwitchCompat
+import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,17 +14,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val switchSummation: SwitchCompat = findViewById(R.id.switchSummation)
+        val switchSubtracting: SwitchCompat = findViewById(R.id.switchSubtracting)
+        val switchMultiplication: SwitchCompat = findViewById(R.id.switchMultiplication)
+        val switchDivision: SwitchCompat = findViewById(R.id.switchDivision)
+
+        if (File("data.txt").exists()) {
+            var file = File("data.txt").bufferedReader()
+            switchSummation.isChecked = file.readLine().toBoolean()
+            switchSubtracting.isChecked = file.readLine().toBoolean()
+            switchMultiplication.isChecked = file.readLine().toBoolean()
+            switchDivision.isChecked = file.readLine().toBoolean()
+        }
+
         val buttonStart: Button = findViewById(R.id.buttonStart)
         buttonStart.setOnClickListener {
             val intent = Intent(this@MainActivity, GameActivity::class.java)
             this@MainActivity.startActivity(intent)
+            File("data.txt").createNewFile()
+            var file = File("data.txt").bufferedWriter()
+            file.write(switchSummation.isChecked.toString())
+            file.newLine()
+            file.write(switchSubtracting.isChecked.toString())
+            file.newLine()
+            file.write(switchMultiplication.isChecked.toString())
+            file.newLine()
+            file.write(switchDivision.isChecked.toString())
+            file.close()
         }
 
         val buttonSettings: ImageButton = findViewById(R.id.buttonSettings)
         buttonSettings.setOnClickListener {
             val intent = Intent(this@MainActivity, SettingsActivity::class.java)
             this@MainActivity.startActivity(intent)
+            File("data.txt").createNewFile()
+            var file = File("data.txt").bufferedWriter()
+            file.write(switchSummation.isChecked.toString())
+            file.newLine()
+            file.write(switchSubtracting.isChecked.toString())
+            file.newLine()
+            file.write(switchMultiplication.isChecked.toString())
+            file.newLine()
+            file.write(switchDivision.isChecked.toString())
+            file.close()
         }
-
     }
 }
